fd39074afaafe2019d96ca07a75fcab810e99873
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-08-13 12:33:53 +0530

    Updated Code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 fn main() {
-    let _number = 5;
-    
+    let _number = 6;
+    print!("The Final number is {}", double_if_even(_number));
+    print!("{}", _number);
 }
 fn double_if_even(number : i32) -> i32{
     let new_number;
     if number %2 == 0{
-        new_number= _number*2;
+        new_number= number*2;
     }
     else {
-        new_number = _number;
+        new_number = number;
     }
     return new_number;
 }
