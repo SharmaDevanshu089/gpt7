c040be6419ce164088c781e7be01be4ffc9599fe
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-08-13 12:31:15 +0530

    Created Most of the Program

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let _number = 5;
+    
+}
+fn double_if_even(number : i32) -> i32{
+    let new_number;
+    if number %2 == 0{
+        new_number= _number*2;
+    }
+    else {
+        new_number = _number;
+    }
+    return new_number;
+}
